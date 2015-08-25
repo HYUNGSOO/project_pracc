e5e43c60b79c7d77e3113de41523158753adf5b0
YOON HYUNGSOO <yhyungs66@gmail.com>
2015-08-25 13:02:21 +0000

    confirmable question

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825105324) do
+ActiveRecord::Schema.define(version: 20150825121043) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
